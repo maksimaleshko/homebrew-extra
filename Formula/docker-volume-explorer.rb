6afc734aa3f6b9b886e655643f813016adb17a57
Maksim Aleshko <maksim.aleshko@higher-order.tech>
2018-03-30 15:28:03 +0300

    docker-volume-explorer.rb updated

--- Formula/docker-volume-explorer.rb
+++ Formula/docker-volume-explorer.rb
@@ -3,6 +3,7 @@ class DockerVolumeExplorer < Formula
     homepage "https://github.com/maksimaleshko/docker-volume-explorer"
     url "https://raw.githubusercontent.com/maksimaleshko/docker-volume-explorer/master/docker-volume-explorer.sh"
     sha256 "66be7b17febf7ad8f991bce64efd869f0029096674213ca5f8fa69d687d34912"
+    version "1.0.0"
   
     depends_on "docker" => :optional
 
